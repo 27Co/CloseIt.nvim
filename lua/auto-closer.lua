27c10647db0b3eb05460e1f8424d2a310e7744dd
BumbleBee <benjamin-_-1111@outlook.com>
2024-08-29 19:23:53 +0800

    Can now close ' " ` normally

--- lua/auto-closer.lua
+++ lua/auto-closer.lua
@@ -13,30 +13,43 @@ local function check_insertion()
   end
 end
 
+local brackets={
+  ["("]=")",
+  ["["]="]",
+  ["{"]="}"
+}
+
+local quotes={
+  ["'"]={true},
+  ['"']={true},
+  ["`"]={true}
+}
+
 local function check_brackets()
   if not check_insertion() then
     return
   end
 
-  local brackets={
-    ["("]=")",
-    ["["]="]",
-    ["{"]="}",
-  }
   local line = vim.api.nvim_get_current_line()
   local col = vim.api.nvim_win_get_cursor(0)[2]
   local char = line:sub(col, col)
   if brackets[char] then
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(brackets[char], true, false, true), 'n', true)
+  elseif quotes[char] then
+    if quotes[char][1] then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(char, true, false, true), 'n', true)
+    end
+    quotes[char][1]=not quotes[char][1]
   end
   return
 end
 
 function auto_closer.setup()
-    vim.api.nvim_create_autocmd("TextChangedI", {
-        pattern = "*",
-        callback = check_brackets
-    })
+  vim.api.nvim_create_autocmd("TextChangedI", {
+    pattern = "*",
+    callback = check_brackets
+  })
 end
 
 return auto_closer
+
