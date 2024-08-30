94ffa449a08f6539fdcd70c6068054a469dfa47f
BumbleBee <benjamin-_-1111@outlook.com>
2024-08-30 14:39:50 +0800

    Can now delete brackets in pairs (without check)

--- lua/auto-closer.lua
+++ lua/auto-closer.lua
@@ -1,20 +1,27 @@
 local auto_closer = {}
 
-local prevCol
+local currRow, currCol=unpack(vim.api.nvim_win_get_cursor(0))
+local prevRow, prevCol
+local currChar=""
+local prevChar
 
-local function init_prevCol()
-  prevCol=vim.api.nvim_win_get_cursor(0)[2]
-end
-
-local function check_forward()
-  local currCol=vim.api.nvim_win_get_cursor(0)[2]
-  if prevCol<currCol then
-    prevCol=currCol
-    return true
-  else
-    prevCol=currCol
-    return false
-  end
+-- update cursor position
+local function get_pos()
+    print("--------------------")
+    -- get_cursor: top right corner is (1, 0)
+    prevRow, prevCol = currRow, currCol
+    currRow, currCol = unpack(vim.api.nvim_win_get_cursor(0))
+    prevChar = currChar
+    if currCol==0 then
+        print("start of line")
+        currChar=""
+    else
+        print("getting char at row:", currRow-1, "column:["..currCol-1, currCol.."]")
+        currChar = vim.api.nvim_buf_get_text(0, currRow-1, currCol-1, currRow-1, currCol, {})[1]
+    end
+    print("cursor at row:", currRow, "column:", currCol)
+    print("prevChar:<"..prevChar..">")
+    print("currChar:<"..currChar.."> at row:", currRow-1, "column:["..currCol-1, currCol.."]")
 end
 
 local brackets={
@@ -29,46 +36,58 @@ local quotes={
   ["`"]={true}
 }
 
-local textChangedI=false
-
-local function text_changed()
-  textChangedI=true
-end
-
 local function close_it()
-  if not textChangedI then
-    return
-  end
-  textChangedI=false
-
-  if not check_forward() then
-    return
-  end
-
-  local line = vim.api.nvim_get_current_line()
-  local col = vim.api.nvim_win_get_cursor(0)[2]
-  local char = line:sub(col, col)
-  if brackets[char] then
-    vim.api.nvim_input(brackets[char].."<Left>")
-  elseif quotes[char] then
-    if quotes[char][1] then
-        vim.api.nvim_input(char.."<Left>")
+  print("~~~~~~~~~~~~~~~~~~~~")
+  -- check single character change
+  local singleAdd, singleRmv=false, false
+  if currRow==prevRow then
+    print("check:", currCol, prevCol)
+    if currCol==prevCol+1 then
+      -- insert a char
+      print("singleAdd")
+      singleAdd=true
+    elseif currCol==prevCol-1 then
+      -- delete a char
+      print("singleRmv")
+      singleRmv=true
     end
-    quotes[char][1]=not quotes[char][1]
   end
-  return
+
+  -- if not, do nothing
+  if not (singleAdd or singleRmv) then
+    print("singleAdd:", singleAdd, "singleRmv:", singleRmv)
+    print("not singleAdd or singleRmv")
+    return
+  end
+
+  -- from here, a single character is either added or removed
+  if singleAdd then
+    if brackets[currChar] then
+      vim.api.nvim_input(brackets[currChar].."<Left>")
+    elseif quotes[currChar] then
+      if quotes[currChar][1] then
+        vim.api.nvim_input(currChar.."<Left>")
+      end
+      quotes[currChar][1]=not quotes[currChar][1]
+    end
+  elseif singleRmv then
+    if brackets[prevChar] then
+      vim.api.nvim_input("<Del>")
+    elseif quotes[prevChar] then
+      if quotes[prevChar][1] then
+        vim.api.nvim_input("<Del>")
+      end
+      quotes[prevChar][1]=not quotes[prevChar][1]
+    end
+  end
 end
 
 function auto_closer.setup()
-  vim.api.nvim_create_autocmd("InsertEnter", {
+  vim.api.nvim_create_autocmd({"InsertEnter", "TextChangedI"}, {
     pattern = "*",
-    callback = init_prevCol
+    callback = get_pos
   })
   vim.api.nvim_create_autocmd("TextChangedI", {
-    pattern = "*",
-    callback = text_changed
-  })
-  vim.api.nvim_create_autocmd("CursorMovedI", {
     pattern = "*",
     callback = close_it
   })
