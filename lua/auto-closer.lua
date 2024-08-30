8cb0d0aaca583b2885399610313346548d13277e
BumbleBee <benjamin-_-1111@outlook.com>
2024-08-30 15:25:38 +0800

    Added check for deletion in pairs; removed quotes

--- lua/auto-closer.lua
+++ lua/auto-closer.lua
@@ -4,14 +4,15 @@ local currRow, currCol=unpack(vim.api.nvim_win_get_cursor(0))
 local prevRow, prevCol
 local currChar=""
 local prevChar
+local posChar=""
 
--- update cursor position
-local function get_pos()
+local function update_pos()
     print("--------------------")
     -- get_cursor: top right corner is (1, 0)
     prevRow, prevCol = currRow, currCol
     currRow, currCol = unpack(vim.api.nvim_win_get_cursor(0))
     prevChar = currChar
+    posChar = vim.api.nvim_buf_get_text(0, currRow-1, currCol, currRow-1, currCol+1, {})[1]
     if currCol==0 then
         print("start of line")
         currChar=""
@@ -30,25 +31,25 @@ local brackets={
   ["{"]="}"
 }
 
+--[[
 local quotes={
   ["'"]={true},
   ['"']={true},
   ["`"]={true}
 }
+--]]
 
 local function close_it()
   print("~~~~~~~~~~~~~~~~~~~~")
   -- check single character change
   local singleAdd, singleRmv=false, false
   if currRow==prevRow then
-    print("check:", currCol, prevCol)
+    print("check:", prevCol, "->", currCol)
     if currCol==prevCol+1 then
       -- insert a char
-      print("singleAdd")
       singleAdd=true
     elseif currCol==prevCol-1 then
       -- delete a char
-      print("singleRmv")
       singleRmv=true
     end
   end
@@ -62,30 +63,36 @@ local function close_it()
 
   -- from here, a single character is either added or removed
   if singleAdd then
+    print("singleAdd")
     if brackets[currChar] then
       vim.api.nvim_input(brackets[currChar].."<Left>")
+    --[[
     elseif quotes[currChar] then
       if quotes[currChar][1] then
         vim.api.nvim_input(currChar.."<Left>")
       end
       quotes[currChar][1]=not quotes[currChar][1]
+    --]]
     end
   elseif singleRmv then
-    if brackets[prevChar] then
+    print("singleRmv")
+    if posChar==brackets[prevChar] then
       vim.api.nvim_input("<Del>")
+    --[[
     elseif quotes[prevChar] then
       if quotes[prevChar][1] then
         vim.api.nvim_input("<Del>")
       end
       quotes[prevChar][1]=not quotes[prevChar][1]
+    --]]
     end
   end
 end
 
 function auto_closer.setup()
-  vim.api.nvim_create_autocmd({"InsertEnter", "TextChangedI"}, {
+  vim.api.nvim_create_autocmd({"TextChangedI", "InsertEnter"}, {
     pattern = "*",
-    callback = get_pos
+    callback = update_pos
   })
   vim.api.nvim_create_autocmd("TextChangedI", {
     pattern = "*",
