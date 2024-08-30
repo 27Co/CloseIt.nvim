609abfcfd3be3f94d70d4e9c08c3618d833b768b
BumbleBee <benjamin-_-1111@outlook.com>
2024-08-30 20:22:56 +0800

    Typing right brackets can now get out

--- lua/auto-closer.lua
+++ lua/auto-closer.lua
@@ -2,24 +2,24 @@ local auto_closer = {}
 
 local currRow, currCol=unpack(vim.api.nvim_win_get_cursor(0))
 local prevRow, prevCol
-local currChar=""
-local prevChar
-local posChar=""
+local currChar=""   -- character right before cursor (current)
+local prevChar      -- character right before cursor (previous)
+local posChar=""    -- character right after cursor (current)
 
 local function update_pos()
   -- get_cursor: top right corner is (1, 0)
   prevRow, prevCol = currRow, currCol
   currRow, currCol = unpack(vim.api.nvim_win_get_cursor(0))
   prevChar = currChar
-  posChar = vim.api.nvim_buf_get_text(0, currRow-1, currCol, currRow-1, currCol+1, {})[1]
   if currCol==0 then
     currChar=""
   else
     currChar = vim.api.nvim_buf_get_text(0, currRow-1, currCol-1, currRow-1, currCol, {})[1]
   end
+  posChar = vim.api.nvim_buf_get_text(0, currRow-1, currCol, currRow-1, currCol+1, {})[1]
 end
 
-local them={
+local lefts={
   ["("]=")",
   ["["]="]",
   ["{"]="}",
@@ -28,11 +28,21 @@ local them={
   ["`"]="`"
 }
 
+local rights={
+  [")"]=true,
+  ["]"]=true,
+  ["}"]=true,
+}
+
 local function close_it()
   local change=currCol-prevCol
-  if change==1 and them[currChar] then
-    vim.api.nvim_buf_set_text(0, currRow-1, currCol, currRow-1, currCol, {them[currChar]})
-  elseif change==-1 and posChar==them[prevChar] then
+  if change==1 then
+    if lefts[currChar] then
+      vim.api.nvim_buf_set_text(0, currRow-1, currCol, currRow-1, currCol, {lefts[currChar]})
+    elseif rights[currChar] and posChar==currChar then
+      vim.api.nvim_buf_set_text(0, currRow-1, currCol, currRow-1, currCol+1, {})
+    end
+  elseif change==-1 and posChar==lefts[prevChar] then
     vim.api.nvim_buf_set_text(0, currRow-1, currCol, currRow-1, currCol+1, {})
   end
 end
