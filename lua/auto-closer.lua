88c73a9700e70400a0b8e9b292c15ef0a8631653
BumbleBee <benjamin-_-1111@outlook.com>
2024-08-30 21:00:55 +0800

    Added support for quotes

--- lua/auto-closer.lua
+++ lua/auto-closer.lua
@@ -23,9 +23,6 @@ local lefts={
   ["("]=")",
   ["["]="]",
   ["{"]="}",
-  ["'"]="'",
-  ['"']='"',
-  ["`"]="`"
 }
 
 local rights={
@@ -34,15 +31,28 @@ local rights={
   ["}"]=true,
 }
 
+local quotes={
+  ["'"]=true,
+  ['"']=true,
+  ["`"]=true
+}
+
 local function close_it()
   local change=currCol-prevCol
   if change==1 then
     if lefts[currChar] then
+      -- opening bracket inserted, should close it
       vim.api.nvim_buf_set_text(0, currRow-1, currCol, currRow-1, currCol, {lefts[currChar]})
+    elseif quotes[currChar] then
+      -- numBool: 1 if posChar is different from currChar (should close quote), 0 otherwise
+      numBool=(posChar~=currChar) and 1 or 0
+      vim.api.nvim_buf_set_text(0, currRow-1, currCol, currRow-1, currCol+1-numBool, {currChar:rep(numBool)})
     elseif rights[currChar] and posChar==currChar then
+      -- closing bracket inserted and next char is the same, should skip it
       vim.api.nvim_buf_set_text(0, currRow-1, currCol, currRow-1, currCol+1, {})
     end
   elseif change==-1 and posChar==lefts[prevChar] then
+    -- opening bracket deleted and closing bracket is right after cursor, should delete it
     vim.api.nvim_buf_set_text(0, currRow-1, currCol, currRow-1, currCol+1, {})
   end
 end
