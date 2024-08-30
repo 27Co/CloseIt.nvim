8d5db39f7fbea00ba5535961f2f0c29efe5e7d1d
BumbleBee <benjamin-_-1111@outlook.com>
2024-08-30 15:37:02 +0800

    Removed messages

--- lua/auto-closer.lua
+++ lua/auto-closer.lua
@@ -7,22 +7,16 @@ local prevChar
 local posChar=""
 
 local function update_pos()
-    print("--------------------")
     -- get_cursor: top right corner is (1, 0)
     prevRow, prevCol = currRow, currCol
     currRow, currCol = unpack(vim.api.nvim_win_get_cursor(0))
     prevChar = currChar
     posChar = vim.api.nvim_buf_get_text(0, currRow-1, currCol, currRow-1, currCol+1, {})[1]
     if currCol==0 then
-        print("start of line")
         currChar=""
     else
-        print("getting char at row:", currRow-1, "column:["..currCol-1, currCol.."]")
         currChar = vim.api.nvim_buf_get_text(0, currRow-1, currCol-1, currRow-1, currCol, {})[1]
     end
-    print("cursor at row:", currRow, "column:", currCol)
-    print("prevChar:<"..prevChar..">")
-    print("currChar:<"..currChar.."> at row:", currRow-1, "column:["..currCol-1, currCol.."]")
 end
 
 local brackets={
@@ -40,11 +34,9 @@ local quotes={
 --]]
 
 local function close_it()
-  print("~~~~~~~~~~~~~~~~~~~~")
   -- check single character change
   local singleAdd, singleRmv=false, false
   if currRow==prevRow then
-    print("check:", prevCol, "->", currCol)
     if currCol==prevCol+1 then
       -- insert a char
       singleAdd=true
@@ -56,14 +48,11 @@ local function close_it()
 
   -- if not, do nothing
   if not (singleAdd or singleRmv) then
-    print("singleAdd:", singleAdd, "singleRmv:", singleRmv)
-    print("not singleAdd or singleRmv")
     return
   end
 
   -- from here, a single character is either added or removed
   if singleAdd then
-    print("singleAdd")
     if brackets[currChar] then
       vim.api.nvim_input(brackets[currChar].."<Left>")
     --[[
@@ -75,7 +64,6 @@ local function close_it()
     --]]
     end
   elseif singleRmv then
-    print("singleRmv")
     if posChar==brackets[prevChar] then
       vim.api.nvim_input("<Del>")
     --[[
